2c1a68651ab143e7443aa497be15ca3433583ee2
kumar.aadarsh <kumar.aadarsh98@gmail.com>
2022-08-03 19:13:17 +0530

    Added to git

---
+++ springboot-backend/src/main/java/com/project/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.project.springboot.repository;
+
+import com.project.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
